cce27ca85d4fc0c60cc623719b8b4159bdc5f930
Batrocc <106jai@gmail.com>
2024-08-07 00:05:56 +0530

    Updated pycursor.py

--- pycursor.py
+++ pycursor.py
@@ -6,7 +6,7 @@ import firebase_admin
 from firebase_admin import credentials
 
 # Initialize Firebase Admin SDK
-cred = credentials.Certificate("C:/Users/jai10/Downloads/mouse-53e0d-firebase-adminsdk-keg1t-59614be5cb.json")
+cred = credentials.Certificate("path/to/serviceAccountKey.json")
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://mouse-53e0d-default-rtdb.asia-southeast1.firebasedatabase.app/'
 })
